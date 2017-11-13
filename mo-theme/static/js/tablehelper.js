ba7b4a010ef7264df163732308646df56b0a42ee
Mo Kweon <kkweon@gmail.com>
2017-11-13 03:02:04 -0800

    AutoUpdate

--- mo-theme/static/js/tablehelper.js
+++ mo-theme/static/js/tablehelper.js
@@ -6,7 +6,7 @@
 function addMdlTableClass() {
   var tables = document.querySelectorAll("table");
   tables.forEach(function(table) {
-    var className = "mdl-data-table mdl-js-data-table mdl-shadow--2dp";
+    var className = "table table-responsive";
     if (!table.className.includes(className)) {
       table.className += " " + className;
     }
